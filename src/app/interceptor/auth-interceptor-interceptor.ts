2928b81bf8844668ab995b39f437f60b20ecf2c6
rap002 <rahees.vp@ibm.com>
2026-05-08 10:41:30 +0530

    added auth mod

--- src/app/interceptor/auth-interceptor-interceptor.ts
+++ src/app/interceptor/auth-interceptor-interceptor.ts
@@ -1,5 +1,19 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
+const PUBLIC_ENDPOINTS=["api/v1/auth/login"]
+
 export const authInterceptorInterceptor: HttpInterceptorFn = (req, next) => {
+  const url=req.url
+  const parent=url.split("/")[5]
+  if (!PUBLIC_ENDPOINTS.some(val => val.includes(parent))){//consider reg ex based updation
+    const token=sessionStorage.getItem("auth")
+    if (token){
+      req = req.clone({
+        setHeaders: {
+          Authorization: `Bearer ${token}`  // note the space
+        }
+      });
+    }
+  }
   return next(req);
 };
---
+++ src/app/service/auth/auth-service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import path from 'path';
+import { map } from 'rxjs';
+
+export interface TokenResponse {
+  token: string;
+  tokenType: string;   // "Bearer"
+  expiresIn: number;   // milliseconds
+  username: string;
+}
+const BASE_URL=environment.baseUrl+"/api/v1/auth"
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(
+    private readonly client:HttpClient
+  ){}
+  login=(username:string,password:string)=>{
+    const path=`${BASE_URL}/login`
+    return this.client.post<TokenResponse>(path,
+      {
+        "username": username,
+        "password": password
+      }).pipe(
+      map((val)=>{
+        sessionStorage.setItem("auth",val.token)
+        return val
+      })
+    )
+  }
+}
+export const isLoggedIn=()=>{
+  if (sessionStorage.getItem("auth")){ return true}
+  return false
+}
